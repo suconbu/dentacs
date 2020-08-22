8e4f8efb95f23f24195e8db7a2bd62cdf0d72425
miwa <suconbu@gmail.com>
2020-08-22 19:18:47 +0900

    Add support for specifying timespan over 24 hours

--- Dentacs/Calculator/DateTimeUtility.cs
+++ Dentacs/Calculator/DateTimeUtility.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Suconbu.Dentacs
 {
     public class DateTimeUtility
     {
-        private static string[] withTimeZoneFormats =
+        private static readonly string[] isoDateTimeWithTimeZoneFormats =
         {
             "yyyy-MM-ddTHH:mm:sszzz",
             "yyyy-MM-ddTHH:mm:sszz",
@@ -33,7 +34,7 @@ namespace Suconbu.Dentacs
             "yyyyMMddTHHmmZ",
             "yyyyMMddTHHZ",
         };
-        private static string[] withoutTimeZoneformats =
+        private static readonly string[] isoDateTimeWithoutTimeZoneformats =
         {
             "yyyy-MM-ddTHH:mm:ss",
             "yyyy-MM-ddTHH:mm",
@@ -46,7 +47,9 @@ namespace Suconbu.Dentacs
             "yyyyMMddTHHmm",
             "yyyyMMddTHH",
             "yyyyMMdd",
-
+        };
+        private static readonly string[] generalDateTimeFormats =
+        {
             "yyyy/M/d H:m:s",
             "yyyy/M/d H:m",
             "yyyy/M/d H",
@@ -64,33 +67,50 @@ namespace Suconbu.Dentacs
             "M/d H",
             "M/d",
         };
+        private static readonly Regex colonSeparatedTimeRegex = new Regex(@"^(\d{1,2})(?::(\d{1,2})(?::(\d{1,2}))?)?$");
 
-        public static DateTime Parse(string s)
+        public static DateTime Parse(string input)
         {
-            if (DateTimeUtility.TryParseDateTime(s, out var result))
+            if (DateTimeUtility.TryParseDateTime(input, out var result))
             {
                 return result;
             }
             throw new FormatException();
         }
 
-        public static bool TryParseDateTime(string s, out DateTime result)
+        public static bool TryParseDateTime(string input, out DateTime result)
         {
-            if (DateTimeOffset.TryParseExact(s, DateTimeUtility.withTimeZoneFormats, CultureInfo.InvariantCulture, DateTimeStyles.None, out var date))
+            var culture = CultureInfo.InvariantCulture;
+            var style = DateTimeStyles.None;
+            if (DateTimeOffset.TryParseExact(input, DateTimeUtility.isoDateTimeWithTimeZoneFormats, culture, style, out var date))
             {
                 result = date.UtcDateTime;
                 return true;
             }
-            if (DateTime.TryParseExact(s, DateTimeUtility.withoutTimeZoneformats, CultureInfo.InvariantCulture, DateTimeStyles.None, out result))
+            if (DateTime.TryParseExact(input, DateTimeUtility.isoDateTimeWithoutTimeZoneformats, culture, style, out result))
+            {
+                return true;
+            }
+            if (DateTime.TryParseExact(input, DateTimeUtility.generalDateTimeFormats, culture, style, out result))
             {
                 return true;
             }
             return false;
         }
 
-        public static bool TryParseTimeSpan(string s, out TimeSpan result)
+        public static bool TryParseTimeSpan(string input, out TimeSpan result)
         {
-            return TimeSpan.TryParse(s, CultureInfo.InvariantCulture, out result);
+            // If this function call do not put first, the hours part might be parsed as days like as:
+            // 48:10:20 -> 48days + 10:20:00 (Not as expected)
+            if (DateTimeUtility.TryParseCustomTimeSpan(input, out result))
+            {
+                return true;
+            }
+            if (TimeSpan.TryParse(input, CultureInfo.InvariantCulture, out result))
+            {
+                return true;
+            }
+            return false;
         }
 
         public static DateTime DateTimeFromSeconds(decimal seconds)
@@ -111,6 +131,27 @@ namespace Suconbu.Dentacs
         public static decimal TimeSpanToSeconds(TimeSpan t)
         {
             return (decimal)t.Ticks / TimeSpan.TicksPerSecond;
+        }
+
+        private static bool TryParseCustomTimeSpan(string input, out TimeSpan result)
+        {
+            result = TimeSpan.Zero;
+            var match = DateTimeUtility.colonSeparatedTimeRegex.Match(input);
+            if (match.Success)
+            {
+                // 26:00:00 -> 1day + 02:00:00
+                var h = match.Groups[1].Value;
+                var m = match.Groups[2].Value;
+                var s = match.Groups[3].Value;
+                var hi = int.Parse(h);
+                var mi = string.IsNullOrEmpty(m) ? 0 : int.Parse(m);
+                var si = string.IsNullOrEmpty(s) ? 0 : int.Parse(s);
+                var days = hi / 60;
+                hi %= 60;
+                result = new TimeSpan(days, hi, mi, si);
+                return true;
+            }
+            return false;
         }
     }
 }
